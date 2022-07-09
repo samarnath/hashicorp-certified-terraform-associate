197fe00f07a50929b99d7700e112f5811890306b
samarnath <samarnath.gande@gmail.com>
2022-07-09 14:31:24 -0700

    Update details of Ec2 instance demo1

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 # Resource Block
-resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+resource "aws_instance" "ec2sam" {
+  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
